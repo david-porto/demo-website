759023fa6c0b30d962ab17b03720999f71cb93da
David Porto <dvdxto97@gmail.com>
2021-08-13 18:16:36 -0600

    Delete 07-Omnifood-Desktop directory

--- 07-Omnifood-Desktop/mobile-nav.js
+++
@@ -1,7 +0,0 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headeEl = document.querySelector(".heade");
-
-btnNavEl.addEventListener("click", function(){
-headeEl.classList.toggle("nav-open")
-
-});
