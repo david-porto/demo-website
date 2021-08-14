0082a55c875ee11be300ed874caee23caff956f0
David Porto <dvdxto97@gmail.com>
2021-08-13 18:17:31 -0600

    Add files via upload

---
+++ mobile-nav.js
@@ -0,0 +1,7 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headeEl = document.querySelector(".heade");
+
+btnNavEl.addEventListener("click", function(){
+headeEl.classList.toggle("nav-open")
+
+});
